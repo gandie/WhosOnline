22651bbf9095cfbe3200debd4ed0c839f7b3bc46
Lars Bergmann <lars.bergmann@perfact.de>
2017-12-22 16:54:57 +0100

    Added arping function analogue to ping function. Both now use safe_syscall function to fire shell commmands. Network device is now determined by given network address using routing information.

--- whosonline/netcheck.py
+++ whosonline/netcheck.py
@@ -29,23 +29,89 @@ import subprocess
 import whosonline.logfacility as logfacility
 LOGGER = logfacility.build_logger()
 
+# some hosts may not have to be checked
+OMIT_HOSTS = ['fritz', ]
+
+
+def safe_syscall(cmds, raisemode=False):
+    '''Send a command and return both the return code and the output.  If
+    used as a compatibility replacement for os.system(), pass
+    "raisemode=True", which will mean that you don't get the return
+    code, but instead an error.
+    '''
+    # Split command into list for safer parsing.
+    if isinstance(cmds, str):
+        cmds = cmds.split()
+
+    import subprocess
+    try:
+        output = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
+        retcode = 0
+    except subprocess.CalledProcessError as e:
+        retcode = e.returncode
+        output = e.output
+
+    if raisemode and retcode:
+        raise AssertionError("safe_syscall returned %s on %s" % (retcode, cmds))
+
+    return retcode, output
+
+
+def get_routes():
+    '''
+    returns list of dictionaries containing routes on system
+    '''
+
+    # route -n parsed by hand
+    returncode, output = safe_syscall(['route', '-n'])
+    routes_raw = output.decode('utf-8').split('\n')[2:]
+    for line_raw in routes_raw:
+        line = line_raw.split()
+        line = [word for word in line if word]
+        if not line:
+            continue
+        route = {
+            'Destination': line[0],
+            'Gateway': line[1],
+            'Genmask': line[2],
+            'Interface': line[-1]
+        }
+        yield route
+
+
+def get_netdevice(network_address):
+    '''get network device which is associated to the given network'''
+    for route in get_routes():
+        if route['Destination'] == network_address:
+            return route['Interface']
+
 
 def notifiy(hostname, status):
-    '''using notify-send shell command to visualize online status nicely'''
+    '''using notify-send shell command to visualize online status nicely. Other
+    methods of notification (e.g. mail) may be implemented here.'''
     cmd = ['notify-send', hostname, 'Online' if status is True else 'Offline']
-    child = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    # just fire command, no response check for now
-    child.communicate()
+    returncode, output = safe_syscall(cmd)
 
 
 def ping(hostname):
     '''use ping shell command to determine if device behind hostname is online
     '''
-    command = "ping -c 1 " + hostname
-    child = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    result_text = child.communicate()[0]
-    result = child.returncode
-    if result == 0:
+    # send 5 packets and wait for a maximum of 5 seconds for the response
+    command = "ping -c 5 -w 5 " + hostname
+    returncode, output = safe_syscall(command.split())
+    if returncode == 0:
+        return True
+    else:
+        return False
+
+
+def arping(ip_address, interface):
+    '''use arping shell command to determine if device behind ip-address is
+    online. sends ethernet frames which should not be dropped by firewalls.'''
+    # send 5 packets and wait for a maximum of 5 seconds for the response
+    command = "arping -c 5 -w 5 -I " + interface + "  " + ip_address
+    returncode, output = safe_syscall(command.split())
+    if returncode == 0:
         return True
     else:
         return False
@@ -74,21 +140,37 @@ def get_ips(network_obj):
 
 
 def netcheck_main(network):
-    '''Build network object from network string and shovel ips to check
-    functions
+    '''Main function of netcheck module called from executable
     '''
+
+    # save results to show notifications if something changes
     results = {}
+
+    # set up network object and build ip strings from it using get_ips filter
+    network_obj = ipaddress.ip_network(network, strict=True)
+    ip_addresses = [str(address_obj) for address_obj in get_ips(network_obj)]
+
+    # get the network device responsible for given network
+    netdevice = get_netdevice(network.split('/')[0])  # cut off cidr netmask
+
+    # get local hostname to add to OMIT_HOSTS list
+    local_hostname = socket.gethostname()
+    OMIT_HOSTS.append(local_hostname)
+
+    # abort if not device can be found
+    assert netdevice, "Unable to find network device belonging to network %s" % network
+    print('Device associated to network %s : %s' % (network, netdevice))
+
     # main loop
     while True:
-        network_obj = ipaddress.ip_network(network, strict=True)
-        for address_obj in get_ips(network_obj):
-            ip_str = str(address_obj)
+        for ip_str in ip_addresses:
             hostname = get_hostname(ip_str)
-            if hostname:
+            if hostname and hostname not in OMIT_HOSTS:
                 last_result = results.get(hostname)
-                ping_result = ping(hostname)
+                ping_result = arping(ip_str, netdevice)  # or use ping(hostname)
                 # notify if status changes
                 if (last_result is not None) and (last_result != ping_result):
                     notifiy(hostname, ping_result)
+                # save new result
                 results[hostname] = ping_result
                 LOGGER.info('Host %s online: %s' % (hostname, ping_result))
